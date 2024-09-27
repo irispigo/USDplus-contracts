daf30e113b3f58b02fd9a154df672423f9ffc644
irispigo <162714059+irispigo@users.noreply.github.com>
2024-09-27 08:44:05 -0700

    Update ITransferRestrictor.sol

--- src/ITransferRestrictor.sol
+++ src/ITransferRestrictor.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.23;
 
 /// @notice Enforces transfer restrictions
 /// @author Dinari (https://github.com/dinaricrypto/sbt-contracts/blob/main/src/ITransferRestrictor.sol)
