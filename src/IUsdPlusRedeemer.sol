9f947d1a651ed5a3ee79118304389476ac26e048
irispigo <162714059+irispigo@users.noreply.github.com>
2024-10-01 04:41:50 -0700

    Update IUsdPlusRedeemer.sol

--- src/IUsdPlusRedeemer.sol
+++ src/IUsdPlusRedeemer.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.23;
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {AggregatorV3Interface} from "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
