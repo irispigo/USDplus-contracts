ae6bf27855a3e477bb66b3977a12c622cdb739e4
irispigo <162714059+irispigo@users.noreply.github.com>
2024-10-16 02:13:07 -0700

    Update RedeemMulticall.s.sol

--- script/RedeemMulticall.s.sol
+++ script/RedeemMulticall.s.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
 
 import "forge-std/Script.sol";
 import {ERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
